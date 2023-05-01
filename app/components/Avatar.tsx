ad53349244a01760f7a966b6daf445aa77c614f6
Shubham Patrick <ahthneeuhl@gmail.com>
2023-05-01 10:00:31 +0530

    Social Login

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,14 +2,18 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+type TProps = {
+  src: string | null | undefined;
+};
+
+const Avatar: React.FC<TProps> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       height={30}
       width={30}
       alt="avatar"
-      src="/placeholder.jpg"
+      src={src || "/placeholder.jpg"}
     />
   );
 };
